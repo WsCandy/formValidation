3b22f2ca51bc4cabd9ff5786a2562ba86bf8c09b
Samuel - Work <s-bridge@live.co.uk>
2014-06-20 10:53:54 +0100

    [RegEx] - Updated email RegEx

--- FV.js
+++ FV.js
@@ -278,7 +278,7 @@
 
 			validateEmail: function(email) {
 
-				var validate = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+				var validate = /^([^\s\\]+)@((\[[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
 			    if (validate.test(email)) {
 			        return true;
